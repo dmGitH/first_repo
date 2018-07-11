d3ff39dba31940dc66317eaf15248c2396cb6f59
dmGitH <41084247+dmGitH@users.noreply.github.com>
2018-07-11 08:55:28 +0200

    backup_2001C++(lolepes2017)

---
+++ Rekurziv_Lolepes/src/rekurziv_lolepes/Timeline.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rekurziv_lolepes;
+
+/**
+ *
+ * @author dm
+ */
+class Timeline {
+
+    void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void stop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
